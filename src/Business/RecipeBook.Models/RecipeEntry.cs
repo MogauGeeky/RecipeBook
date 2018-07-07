0e4be0c733b28fe1ca23cabf74c4f9a4341404b2
Kornect <mogauisakm@live.com>
2018-07-07 19:59:37 +0200

    Added recipe book models

---
+++ src/Business/RecipeBook.Models/RecipeEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeBook.Models
+{
+    public class RecipeEntry
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Notes { get; set; }
+
+        public virtual IEnumerable<RecipeEntryStep> RecipeEntrySteps { get; set; }
+    }
+}
---
+++ src/Business/RecipeBook.Models/RecipeEntryStep.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeBook.Models
+{
+    public class RecipeEntryStep
+    {
+        public string Id { get; set; }
+        public int Sequence { get; set; }
+        public string RecipeEntryId { get; set; }
+        public string Notes { get; set; }
+
+        public virtual RecipeEntry RecipeEntry { get; set; }
+    }
+}
