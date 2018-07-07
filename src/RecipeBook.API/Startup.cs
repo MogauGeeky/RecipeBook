3100a51a3a1e5ac50f15b6826963dd070dd8d192
Kornect <mogauisakm@live.com>
2018-07-07 19:37:53 +0200

    Added configuration app builder

--- src/RecipeBook.API/Startup.cs
+++ src/RecipeBook.API/Startup.cs
@@ -15,9 +15,14 @@ namespace RecipeBook.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
--- src/RecipeBook.Angular/Startup.cs
+++ src/RecipeBook.Angular/Startup.cs
@@ -10,9 +10,14 @@ namespace RecipeBook.Angular
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
